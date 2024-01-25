060acc89ddebd5159fdd420444d70aa7bbca66b5
양세연 <104888233+yangsae912@users.noreply.github.com>
2024-01-25 11:44:19 +0900

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -24,25 +24,3 @@ if uploaded_file is not None:
     st.write(string_data)
 
  
- 
-# uploaded_file = st.file_uploader("파일 업로드", type='xlsx', key='file')
-
-# try: 
-#         file = st.file_uploader("파일 업로드", type='xlsx', key='file')
-
-
-
-
-#         if st.session_state.file != None:
-#             with st.container():
-#                 tags = None
-#                 use_tagset = st.radio(label="tagset을 제공하시겠습니까?", 
-#                               options=('아니오', '예'), 
-#                               help='tagset에 포함되지 않은 tag를 식별하기 위해 tagset을 입력합니다.', 
-#                               key='use_tagset')
-#                 if st.session_state.use_tagset =='예':
-#                     tags = st.text_area("tagset을 입력하시오. (No Tag와 Obscure 제외, **각 tag 줄바꿈으로 분리** 필수!)", 
-#                                 key='tags')
-# except Exception as e:
-#         st.error(str(e))
-    
